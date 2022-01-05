72ab3b4bb58f8d10b37b98488032bfc982703319
prs176 <prsx176@gmail.com>
2022-01-05 10:45:03 +0900

    [ADD] not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { memo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './page/NotFound';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path={'*'} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/page/NotFound.tsx
@@ -0,0 +1,12 @@
+import { memo } from "react"
+
+const NotFound = (): JSX.Element => {
+    return (
+        <div>
+            404<br />
+            존재하지 않는 페이지입니다.
+        </div>
+    )
+}
+
+export default memo(NotFound);
