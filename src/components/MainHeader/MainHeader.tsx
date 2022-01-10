1efb9e1130b8e26e9e7c6962d3063d6312bd2494
sungHyun <shp35079@naver.com>
2022-01-10 17:40:17 +0900

    [DELETE] 필요없는 fragment 문법 삭제

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -4,14 +4,12 @@ import * as S from './style';
 
 const MainHeader = () => {
   return (
-    <>
-      <S.Wrapper>
-        <S.Logo src={logo} alt='' />
-        <S.BtnWrapper>
-          <S.AddAccountBtn>계정 추가</S.AddAccountBtn>
-        </S.BtnWrapper>
-      </S.Wrapper>
-    </>
+    <S.Wrapper>
+      <S.Logo src={logo} alt='' />
+      <S.BtnWrapper>
+        <S.AddAccountBtn>계정 추가</S.AddAccountBtn>
+      </S.BtnWrapper>
+    </S.Wrapper>
   );
 };
 
