b899f7d25f7928c00c628b08eb3113b65872d6b3
prs176 <prsx176@gmail.com>
2022-01-10 15:52:10 +0900

    [ADD] service enum

--- src/lib/enum/service.ts
+++ src/lib/enum/service.ts
@@ -1,4 +1,14 @@
 export enum EService {
+    JOBDA = '잡다' as any,
+    JOBDA_CMS = '잡다-CMS' as any
+}
+
+export enum EServiceToNum {
     JOBDA = 0,
     JOBDA_CMS = 1
+}
+
+export enum EServiceToColor {
+    JOBDA = '#D3E5EF',
+    JOBDA_CMS = '#F5E0E9'
 }
