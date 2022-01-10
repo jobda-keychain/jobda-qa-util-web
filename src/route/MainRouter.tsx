cae25f229caf529692a4ca13e2ccb7cf8e90444e
prs176 <prsx176@gmail.com>
2022-01-10 14:50:37 +0900

    [FIX] path

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,13 +1,13 @@
 import React, { memo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from '../components/main/Main';
+import Main from '../page/Main/Main';
 import NotFound from '../page/NotFound/NotFound';
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'*'} element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
         <Route path='/main' element={<Main />} />
       </Routes>
     </BrowserRouter>
