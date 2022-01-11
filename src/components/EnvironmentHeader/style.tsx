6583b31953ca30e1e25f2af79f8abbba3f9f8f08
prs176 <prsx176@gmail.com>
2022-01-11 15:15:53 +0900

    [ADD] environment header

---
+++ src/components/EnvironmentHeader/style.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const EnvironmentGroup = styled.div`
+    width: 10%;
+    font-weight: bold;
+`
+
+export const PlatformGroup = styled.div`
+    width: 15%;
+    font-weight: bold;
+`
+
+export const ClientDomainGroup = styled.div`
+    width: 30%;
+    font-weight: bold;
+`
+
+export const ServerDomainGroup = styled.div`
+    width: 30%;
+    font-weight: bold;
+`
+
+export const ButtonGroup = styled.div`
+    float: right;
+    gap: 10px;
+`
