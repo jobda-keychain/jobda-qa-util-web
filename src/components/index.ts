5f047eff038271fff0f3d9e9349dc01676a52dca
sungHyun <shp35079@naver.com>
2022-01-12 16:07:30 +0900

    [ADD] 인덱스 파일에 MainFilter 및 MainSection 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as MainHeader } from './MainHeader/MainHeader';
 export { default as MainTab } from './MainTab/MainTab';
+export { default as MainFilter } from './MainFilter/MainFilter';
+export { default as MainSection } from './MainSection/MainSection';
