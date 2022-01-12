f82699d344eb79284fdb6a4ec8fdde066b1b547f
prs176 <prsx176@gmail.com>
2022-01-12 12:17:04 +0900

    [ADD] h tag style

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -13,6 +13,30 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         font-family: 'Noto Sans KR', sans-serif;
     };
+
+    h1 {
+        font-size: 2em;
+    }
+
+    h2 {
+        font-size: 1.5em;
+    }
+
+    h3 {
+        font-size: 1.17em
+    }
+
+    h4 {
+        font-size: 1em;
+    }
+
+    h5 {
+        font-size: .83em;
+    }
+
+    h6 {
+        font-size: .67em;
+    }
 `;
 
 export default GlobalStyle;
