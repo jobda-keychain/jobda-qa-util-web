f114dc42435d14707e4953fe3a46aae23b4d5c63
prs176 <prsx176@gmail.com>
2022-01-12 16:41:13 +0900

    [MODIFY] help, textfield

--- src/components/CopyModal/CopyModal.tsx
+++ src/components/CopyModal/CopyModal.tsx
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import { ModalButton } from "../../style/Modal";
-import * as S from "./style";
+import { TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { ModalButton } from '../../style/Modal';
+import * as S from './style';
 
 const CopyModal = (): JSX.Element => {
-    const [format, setFormat] = useState('');
+  const [format, setFormat] = useState('');
 
-    const onChangeFormat = (e: React.ChangeEvent<HTMLInputElement>) => setFormat(e.target.value);
+  const onChangeFormat = (e: React.ChangeEvent<HTMLInputElement>) => setFormat(e.target.value);
 
-    return (
-        <S.CopyModal>
-            <div>
-                <h3>클립보드에 복사</h3>
-                
-                <S.HelpWrapper>
-                    <p>환경 = !(Env)</p>
-                    <p>아이디 = !(Id)</p>
-                    <p>비밀번호 = !(Pw)</p>
-                    <p>서비스 = !(Ser)</p>
-                </S.HelpWrapper>
-            </div>
+  return (
+    <S.CopyModal>
+      <div>
+        <h3>클립보드에 복사</h3>
 
-            <div>
-                <h4>포멧</h4>
-                <S.FormatInput
-                    value={format}
-                    placeholder='환경: !(Env), 아이디: !(Id), 비밀번호: !(Pw), 서비스: !(Ser)'
-                    onChange={onChangeFormat} />
-            </div>
+        <S.HelpWrapper>
+          기본 값: '환경: !(Env) 아이디: !(Id) 비밀번호: !(Pw) 서비스: !(Ser)'
+        </S.HelpWrapper>
+      </div>
 
-            <S.ButtonWrapper><ModalButton>복사</ModalButton></S.ButtonWrapper>
-        </S.CopyModal>
-    )
-}
+      <TextField
+        id='filled-basic'
+        label='환경'
+        variant='filled'
+        value={format}
+        onChange={onChangeFormat}
+      />
+
+      <S.ButtonWrapper>
+        <ModalButton>복사</ModalButton>
+      </S.ButtonWrapper>
+    </S.CopyModal>
+  );
+};
 
 export default CopyModal;
