ec65d8f79d45271ff15fc1c0a9e7ff8d8fa15c17
sungHyun <shp35079@naver.com>
2022-01-13 10:50:17 +0900

    [ADD] 환경 관리 버튼 누르면 페이지 이동

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import * as S from './styles';
 import { setting } from '../../assets/Main';
 import { MainFilter, PublicTab } from '..';
+import { useNavigate } from 'react-router-dom';
 
 const MainSection = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Wrapper>
       <S.Header>
         <PublicTab />
-        <S.EnvBtn>
+        <S.EnvBtn onClick={() => navigate('/env-management')}>
           <img src={setting} alt='' />
           <span>환경 관리</span>
         </S.EnvBtn>
