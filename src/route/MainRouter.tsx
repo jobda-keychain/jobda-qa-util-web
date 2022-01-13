62a93cacbf7e3cb5ee27efd7aa5d37213a7ba37f
sungHyun <shp35079@naver.com>
2022-01-13 10:43:42 +0900

    [ADD] 환경관리 페이지 라우팅 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EnvManagement from '../page/EnvManagement/EnvManagement';
 import Main from '../page/Main/Main';
 import NotFound from '../page/NotFound/NotFound';
 
@@ -9,6 +10,7 @@ const MainRouter = () => {
       <Routes>
         <Route path='*' element={<NotFound />} />
         <Route path='/' element={<Main />} />
+        <Route path='/env-management' element={<EnvManagement />} />
       </Routes>
     </BrowserRouter>
   );
