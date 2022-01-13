24b621c8345f6a95cbd2818f67cfa33752d39327
sungHyun <shp35079@naver.com>
2022-01-13 11:23:15 +0900

    [FIX] MainSection 실수로 지운 파일 다시 생성

---
+++ src/components/MainSection/style.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled('section')`
+  margin: 0 200px;
+  background-color: #fff;
+  border-radius: 20px;
+`;
+
+export const Header = styled('header')`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #e7eaf1;
+`;
+
+export const EnvBtn = styled('div')`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  padding: 0 40px;
+  color: #d1d1d1;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+`;
