9a6220ef1cff785e40314a1833e0dc3f61c7f8e7
prs176 <prsx176@gmail.com>
2022-01-13 14:15:32 +0900

    [MODIFY] useState

--- src/components/EnvironmentModal/EnvironmentModal.tsx
+++ src/components/EnvironmentModal/EnvironmentModal.tsx
@@ -2,39 +2,49 @@ import { TextField } from '@mui/material';
 import { useMemo, useState } from 'react';
 import { EPlatform } from '../../lib/enum/platform';
 import { ModalButton } from '../../style/Modal';
+import { IEnvironment } from '../../types/environment.types';
 import ServiceRadio from '../ServiceRadio/ServiceRadio';
 import * as S from './style';
 
 interface EnvironmentModalProps {
   type: 'create' | 'modify';
-  nameValue?: string;
-  clientDomainValue?: string;
-  serverDomainValue?: string;
-  platformValue?: EPlatform;
+  value?: IEnvironment;
 }
 
-const EnvironmentModal = ({
-  type,
-  nameValue,
-  clientDomainValue,
-  serverDomainValue,
-  platformValue,
-}: EnvironmentModalProps): JSX.Element => {
-  const [name, setName] = useState(nameValue ?? '');
-  const [clientDomain, setClientDomain] = useState(clientDomainValue ?? '');
-  const [serverDomain, setServerDomain] = useState(serverDomainValue ?? '');
-  const [platform, setPlatform] = useState(platformValue ?? EPlatform.JOBDA);
+const EnvironmentModal = ({ type, value }: EnvironmentModalProps): JSX.Element => {
+  const [environment, setEnvironment] = useState(
+    value ?? {
+      id: 0,
+      name: '',
+      serverDomain: '',
+      clientDomain: '',
+      platform: EPlatform.JOBDA,
+    },
+  );
 
-  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setEnvironment({
+      ...environment,
+      name: e.target.value,
+    });
 
   const onChangeClientDomain = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setClientDomain(e.target.value);
+    setEnvironment({
+      ...environment,
+      clientDomain: e.target.value,
+    });
 
   const onChangeServerDomain = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setServerDomain(e.target.value);
+    setEnvironment({
+      ...environment,
+      serverDomain: e.target.value,
+    });
 
   const onChangePlatform = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setPlatform(e.target.value as EPlatform);
+    setEnvironment({
+      ...environment,
+      platform: e.target.value as EPlatform,
+    });
 
   const typeLabel = useMemo(() => (type === 'create' ? '생성' : '수정'), [type]);
 
@@ -48,11 +58,11 @@ const EnvironmentModal = ({
             id='filled-basic'
             label='환경 이름'
             variant='filled'
-            value={name}
+            value={environment.name}
             onChange={onChangeName}
           />
           <div>
-            <ServiceRadio platform={platform} onChangePlatform={onChangePlatform} />
+            <ServiceRadio platform={environment.platform} onChangePlatform={onChangePlatform} />
           </div>
         </S.MultipleInputWrapper>
 
@@ -61,7 +71,7 @@ const EnvironmentModal = ({
             id='filled-basic'
             label='클라이언트 도메인'
             variant='filled'
-            value={clientDomain}
+            value={environment.clientDomain}
             onChange={onChangeClientDomain}
           />
         </S.InputWrapper>
@@ -71,7 +81,7 @@ const EnvironmentModal = ({
             id='filled-basic'
             label='서버 도메인'
             variant='filled'
-            value={serverDomain}
+            value={environment.serverDomain}
             onChange={onChangeServerDomain}
           />
         </S.InputWrapper>
