2d62f746bb655bd8e821140064b1d5148b17bf81
sungHyun <shp35079@naver.com>
2022-01-13 11:22:20 +0900

    [ADD] 인덱스 파일에 EnvSection 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as PublicHeader } from './PublicHeader/PublicHeader';
 export { default as PublicTab } from './PublicTab/PublicTab';
 export { default as MainFilter } from './MainFilter/MainFilter';
 export { default as MainSection } from './MainSection/MainSection';
+export { default as EnvSection } from './EnvSection/EnvSection';
