5d3af2367877c22985812d53d83933ab7f83ffc8
sungHyun <shp35079@naver.com>
2022-01-14 12:18:10 +0900

    [MODIFY] 버튼 스타일 변경 (mui)

--- src/components/Modal/DeleteModal/DeleteModal.tsx
+++ src/components/Modal/DeleteModal/DeleteModal.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import React, { FC } from 'react';
 import * as S from './style';
 
@@ -14,8 +15,8 @@ const DeleteModal: FC<Props> = ({ isShowModal, closeModal }) => {
           <S.ModalContainer>
             <span>계정을 삭제하시겠습니까?</span>
             <S.ButtonContainer>
-              <button>예</button>
-              <button>아니오</button>
+              <Button variant='contained'>예</Button>
+              <Button variant='outlined'>아니오</Button>
             </S.ButtonContainer>
           </S.ModalContainer>
         </S.Wrapper>
