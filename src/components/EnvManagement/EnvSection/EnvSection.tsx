993d1cb97edd4070023e028b74985cc8635fc657
sungHyun <shp35079@naver.com>
2022-01-14 15:49:55 +0900

    [FIX] 머지 후 발생하는 에러 해결

--- src/components/EnvManagement/EnvSection/EnvSection.tsx
+++ src/components/EnvManagement/EnvSection/EnvSection.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { PublicTab } from '../..';
+import { useState } from 'react';
 import * as S from './style';
+import { PublicTab } from '../..';
+import { ListWrapper, PaginationtWrapper, SectionWrapper } from '../../../style/Section';
+import StyledPagination from '../../PaginationButton/PaginationButton';
+import { IEnvironment } from '../../../types/environment.types';
+import EnvironmentRow from '../../EnvironmentList/EnvironmentRow';
+import EnvironmentHeader from '../../EnvironmentList/EnvironmentHeader';
+import { EPlatform } from '../../../lib/enum/platform';
 
 const EnvSection = () => {
+  const [pageCount, setPageCount] = useState(1);
+  const [environments, setEnvironments] = useState<IEnvironment[]>([]);
+
   return (
-    <S.Wrapper>
+    <SectionWrapper>
       <S.TabBox>
         <PublicTab />
       </S.TabBox>
-    </S.Wrapper>
+
+      <ListWrapper>
+        <EnvironmentHeader />
+        <hr />
+        {environments.map(environment => (
+          <div key={environment.id}>
+            <EnvironmentRow environment={environment} />
+            <hr />
+          </div>
+        ))}
+      </ListWrapper>
+
+      <PaginationtWrapper>
+        <StyledPagination count={pageCount} />
+      </PaginationtWrapper>
+    </SectionWrapper>
   );
 };
 
