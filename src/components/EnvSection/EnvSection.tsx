5beabd71d7eabd77919f0a0f974e1200d2051c65
prs176 <prsx176@gmail.com>
2022-01-14 09:26:50 +0900

    [ADD] account list

--- src/components/EnvSection/EnvSection.tsx
+++ src/components/EnvSection/EnvSection.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import { useMemo, useState } from 'react';
 import * as S from './style';
 import { PublicTab } from '..';
+import { ListWrapper, PaginationtWrapper, SectionWrapper } from '../../style/Section';
+import StyledPagination from '../PaginationButton/PaginationButton';
+import { IEnvironment } from '../../types/environment.types';
+import EnvironmentRow from '../EnvironmentList/EnvironmentRow';
+import EnvironmentHeader from '../EnvironmentList/EnvironmentHeader';
 
 const EnvSection = () => {
+  const [pageCount, setPageCount] = useState(1);
+  const [environments, setEnvironments] = useState<IEnvironment[]>([]);
+
+  const environmentList = useMemo(
+    () =>
+      environments.map(environment => (
+        <div key={environment.id}>
+          <EnvironmentRow value={environment} />
+          <hr />
+        </div>
+      )),
+    [environments],
+  );
+
   return (
-    <S.Wrapper>
+    <SectionWrapper>
       <S.TabBox>
         <PublicTab />
       </S.TabBox>
-    </S.Wrapper>
+
+      <ListWrapper>
+        <EnvironmentHeader />
+        <hr />
+        {environmentList}
+      </ListWrapper>
+
+      <PaginationtWrapper>
+        <StyledPagination count={pageCount} />
+      </PaginationtWrapper>
+    </SectionWrapper>
   );
 };
 
