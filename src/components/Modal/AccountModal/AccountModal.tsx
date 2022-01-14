197d0ad6641615ce8276fde6bd314fd77eb0f370
sungHyun <shp35079@naver.com>
2022-01-14 14:43:45 +0900

    [MODIFY] 객체를 리턴하는 함수로 변경

--- src/components/Modal/AccountModal/AccountModal.tsx
+++ src/components/Modal/AccountModal/AccountModal.tsx
@@ -20,15 +20,40 @@ const AccountModal: FC<Props> = ({ isShowModal, closeModal, type }) => {
       name: 'st1',
     },
   ]);
-  const isAdd = type === '추가';
-  const isDetail = type === '상세보기';
+
+  const getModalInfo = () => {
+    switch (type) {
+      case 'add':
+        return {
+          title: '추가',
+          buttonText: '추가',
+          isAdd: true,
+          isDetail: false,
+        };
+      case 'modify':
+        return {
+          title: '수정',
+          buttonText: '수정',
+          isAdd: true,
+          isDetail: false,
+        };
+      case 'detail':
+        return {
+          title: '상세보기',
+          buttonText: '닫기',
+          isAdd: false,
+          isDetail: true,
+        };
+    }
+  };
+  const { title, buttonText, isAdd, isDetail } = getModalInfo();
 
   return (
     <>
       {isShowModal && (
         <S.Wrapper onClick={closeModal}>
           <S.ModalContainer onClick={e => e.stopPropagation()}>
-            <S.Title>{`계정 ${type}`}</S.Title>
+            <S.Title>{`계정 ${title}`}</S.Title>
             <S.EnvInput
               size='small'
               disablePortal
@@ -43,7 +68,7 @@ const AccountModal: FC<Props> = ({ isShowModal, closeModal, type }) => {
             </S.AuthInputsContainer>
             <textarea placeholder='상세 설명' disabled={isDetail} />
             <S.ButtonContainer>
-              <button>{type === '상세보기' ? '취소' : type}</button>
+              <button>{buttonText}</button>
             </S.ButtonContainer>
           </S.ModalContainer>
         </S.Wrapper>
