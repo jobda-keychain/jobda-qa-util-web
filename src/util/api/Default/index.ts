8f8870e16adad5513b22703d403d9acd58dc42e2
sungHyun <shp35079@naver.com>
2022-01-17 11:53:49 +0900

    [DELETE] request 삭제 및 timeout 속성 삭제

--- src/util/api/Default/index.ts
+++ src/util/api/Default/index.ts
@@ -1,14 +1,8 @@
 import axios, { AxiosError } from 'axios';
 const baseURL = process.env.REACT_APP_BASE_URL;
 
-export const request = axios.create({
-  baseURL,
-  timeout: 100000,
-});
-
 const instance = axios.create({
   baseURL,
-  timeout: 100000,
 });
 
 instance.interceptors.response.use(
