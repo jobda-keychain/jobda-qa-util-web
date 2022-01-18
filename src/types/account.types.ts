2404c05f545243682b949edbabd255a82a0fc53c
sungHyun <shp35079@naver.com>
2022-01-18 14:01:35 +0900

    [MODIFY] password를 optional로 변경

--- src/types/account.types.ts
+++ src/types/account.types.ts
@@ -3,7 +3,7 @@ import { EPlatform } from '../lib/enum/platform';
 export interface IAccount {
   id: number;
   userId: string;
-  password: string;
+  password?: string;
   platform: EPlatform;
   environment: string;
   description: string;
