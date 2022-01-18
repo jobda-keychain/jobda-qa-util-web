ce2dc776fbcaf414bb588b8c3fe62f3ceaa6d229
prs176 <prsx176@gmail.com>
2022-01-18 15:29:29 +0900

    [REFACTOR] method chaining, separate method

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -6,19 +6,20 @@ const useCopy = (account: Account) => {
 
   const onChangeFormat = (e: React.ChangeEvent<HTMLInputElement>) => setFormat(e.target.value);
 
-  const copy = async () => {
-    let target = format;
-
+  const doFormat = (): string => {
     if (!format) {
-      target = `환경: ${account.environment} 아이디: ${account.id} 비밀번호: ${account.password} 서비스: ${account.platform}`;
+      return `환경: ${account.environment} 아이디: ${account.id} 비밀번호: ${account.password} 서비스: ${account.platform}`;
     } else {
-      target = target.replace('!(Env)', account.environment);
-      target = target.replace('!(Id)', account.userId);
-      target = target.replace('!(Pw)', account.password);
-      target = target.replace('!(Ser)', account.platform);
+      return format
+        .replace('!(Env)', account.environment)
+        .replace('!(Id)', account.userId)
+        .replace('!(Pw)', account.password)
+        .replace('!(Ser)', account.platform);
     }
+  };
 
-    await navigator.clipboard.writeText(target);
+  const copy = async () => {
+    await navigator.clipboard.writeText(doFormat());
   };
 
   return { format, onChangeFormat, copy };
