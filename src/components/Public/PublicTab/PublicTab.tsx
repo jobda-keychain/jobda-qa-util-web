db89356aad1379fa0e1be0589247335070b68e7b
sungHyun <shp35079@naver.com>
2022-01-18 10:15:57 +0900

    [ADD]  부모 컴포넌트에서 탭 값을 받아올 수 있게 추가

--- src/components/Public/PublicTab/PublicTab.tsx
+++ src/components/Public/PublicTab/PublicTab.tsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import * as S from './style';
 
-const PublicTab = () => {
+interface PublicTabProps {
+  updateTab: (tabName: string | null) => void;
+}
+
+const PublicTab: FC<PublicTabProps> = ({ updateTab }) => {
   const [tabNumber, setTabNumber] = useState(0);
 
   const tabHandler = (event: React.SyntheticEvent, value: number) => {
     setTabNumber(value);
   };
+
+  useEffect(() => {
+    switch (tabNumber) {
+      case 0:
+        updateTab(null);
+        break;
+      case 1:
+        updateTab('JOBDA');
+        break;
+      case 2:
+        updateTab('JOBDA_CMS');
+    }
+  }, [tabNumber]);
 
   return (
     <S.Wrapper value={tabNumber} onChange={tabHandler}>
