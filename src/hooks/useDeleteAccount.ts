98063db8c07031897d581764a3a9ca9299dcee07
sungHyun <shp35079@naver.com>
2022-01-19 15:30:47 +0900

    [ADD] 계정 삭제 기능 구현

--- src/hooks/useDeleteAccount.ts
+++ src/hooks/useDeleteAccount.ts
@@ -1,10 +1,15 @@
 import { useCallback } from 'react';
+import { deleteAccount } from './../util/api/Account/index';
 
 const useDeleteAccount = (id: number, onClose: () => void) => {
   const onClickDeleteAccountButton = useCallback(async () => {
     try {
+      await deleteAccount(id);
       onClose();
-    } catch (error: any) {}
+      console.log('삭제에 성공했습니다.');
+    } catch (error: any) {
+      throw error;
+    }
   }, [id, onClose]);
 
   return { onClickDeleteAccountButton };
