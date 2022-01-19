1b0b6e2d3b4165f1f8ab1feb8fd633dd4c6ed4d9
sungHyun <shp35079@naver.com>
2022-01-19 15:43:19 +0900

    [ADD] 삭제를 누를 시 리렌더링 되도록 추가

--- src/hooks/useDeleteAccount.ts
+++ src/hooks/useDeleteAccount.ts
@@ -1,11 +1,12 @@
 import { useCallback } from 'react';
 import { deleteAccount } from './../util/api/Account/index';
 
-const useDeleteAccount = (id: number, onClose: () => void) => {
+const useDeleteAccount = (id: number, onClose: () => void, getAccounts?: () => void) => {
   const onClickDeleteAccountButton = useCallback(async () => {
     try {
       await deleteAccount(id);
       onClose();
+      if (getAccounts) getAccounts();
       console.log('삭제에 성공했습니다.');
     } catch (error: any) {
       throw error;
