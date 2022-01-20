2b83e4231f11702cd36cbd5f828fe095e70960b1
sungHyun <shp35079@naver.com>
2022-01-20 11:25:38 +0900

    [ADD] 환경 리스트 불러오기 및 필터링 api 파일

--- src/util/api/environment/index.ts
+++ src/util/api/environment/index.ts
@@ -1,5 +1,6 @@
 import instance from '../Default';
-import { CreateEnvironmentRequest, ModifyEnvironmentRequest } from './environment.request';
+import { CreateEnvironmentRequest, ModifyEnvironmentRequest } from '../../../models/request';
+import { EnvironmentListResponse } from './../../../models/response/index';
 
 export const createEnvironment = async (request: CreateEnvironmentRequest): Promise<boolean> => {
   const url: string = `environments`;
@@ -20,4 +21,13 @@ export const deleteEnvironment = async (id: number): Promise<boolean> => {
   const url: string = `environments/${id}`;
   const { status } = await instance.delete(url);
   return status === 204;
+};
+
+export const getEnvironmentList = async (page: number, platform?: string | null) => {
+  try {
+    const params = { page, platform, size: 6 };
+    return await instance.get<EnvironmentListResponse>(`/environments`, { params });
+  } catch (error) {
+    throw error;
+  }
 };
