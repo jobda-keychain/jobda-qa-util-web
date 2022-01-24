85a81d5b4c06eed67b9e8bcd36b5f9ee6d346db3
sungHyun <shp35079@naver.com>
2022-01-24 12:21:54 +0900

    [MODIFY] account interface의 key 네이밍 수정
    userId -> accountId

--- src/types/account.types.ts
+++ src/types/account.types.ts
@@ -2,7 +2,7 @@ import { Platform } from '../lib/enum/platform';
 
 export interface Account {
   id: number;
-  userId: string;
+  accountId: string;
   password?: string;
   platform: Platform;
   environment: string;
