4ba8873bb0eafccde76f08338c499f100cf62020
sungHyun <shp35079@naver.com>
2022-01-24 17:54:27 +0900

    [FIX] 존재하지 않는 pk 값에 접근할 경우 모달이 닫히고 리렌더되게 수정

--- src/hooks/useDeleteAccount.ts
+++ src/hooks/useDeleteAccount.ts
@@ -1,20 +1,22 @@
 import { useCallback, useState } from 'react';
-import handleAxiosError from '../util/api/handleAxiosError';
 import { deleteAccount } from './../util/api/Account/index';
+import { AxiosError } from 'axios';
 
 const useDeleteAccount = (id: number, onClose: () => void, getAccounts?: () => void) => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const onClickDeleteAccountButton = useCallback(async () => {
-    handleAxiosError(
-      async () => {
-        await deleteAccount(id);
-        onClose();
-        if (getAccounts) getAccounts();
-      },
-      { 400: '환경에 속한 계정이 있습니다. 환경에 속한 계정을 전부 제거하고 재시도하세요' },
-      setErrorMessage,
-    );
+    try {
+      await deleteAccount(id);
+      onClose();
+      if (getAccounts) getAccounts();
+    } catch (err) {
+      switch ((err as AxiosError).response?.status) {
+        case 404:
+          if (getAccounts) getAccounts();
+          onClose();
+      }
+    }
   }, [id, onClose, getAccounts]);
 
   return { onClickDeleteAccountButton };
