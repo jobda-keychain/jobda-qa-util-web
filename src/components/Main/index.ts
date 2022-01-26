51d85ba9a8661c5005aa89ac5b7a49bf1f19fd6c
sungHyun <shp35079@naver.com>
2022-01-26 15:11:36 +0900

    [CREATE] 메인 export 파일 추가

---
+++ src/components/Main/index.ts
@@ -0,0 +1,4 @@
+export { default as AccountHeader } from './AccountList/AccountHeader';
+export { default as AccountRow } from './AccountList/AccoutRow';
+export { default as MainFilter } from './MainFilter/MainFilter';
+export { default as MainSection } from './MainSection/MainSection';
